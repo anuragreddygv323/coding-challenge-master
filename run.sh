13caf1972fc16ca6d60c40df47e24cc5600b9623
anuragreddygv323 <anuragreddygv323@gmail.com>
2016-04-04 19:42:56 -0400

    My solution for Insight Data Challenge
    Insight Data Challenge

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# example of the run script for running the word count
+
+# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+python ./src/new_final.py
+
