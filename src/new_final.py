42a6c3e58eb634292de1306b1f22df194a4f8a98
anuragreddygv323 <anuragreddygv323@gmail.com>
2016-04-04 20:58:48 -0400

    My Latest code
    My latest code

--- src/new_final.py
+++ src/new_final.py
@@ -1,40 +1,42 @@
-unique_tags=[]
-class Calculate_score(object):
-		
-		""" 
-		Attributes:
-			name: A string representing the customer's name.
-			balance: A float tracking the current balance of the customer's account.
-		"""
+UniCountertags=list()
+def main():
+    import os
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    print file_dir
+    input_file = file_dir + r'/tweet_input/tweets.txt'
+    print input_file
+    output_file = file_dir + r'/tweet_output/output.txt'
+    print output_file
+    Avg_degree_total(input_file,output_file)
 
+class Tweetlvlscore(object):
 		def __init__(self, time,tags):
-			
-			
+
 			self.time = time
-			
 			self.tags = tags
 			self.length=len(tags)
 			self.new_tags=[]
-			global unique_tags 
-			self.new_tags = list(set(tags)-set(unique_tags))
+			global UniCountertags 
+			self.new_tags = list(set(tags)-set(UniCountertags))
 			if self.length != 1:
-				unique_tags = unique_tags+self.new_tags
+				UniCountertags = UniCountertags+self.new_tags
 			self.newtags_length=len(self.new_tags)
 					
 	  
-		def score(self):
+		def Tweetscore(self):
 			self.scores=(self.length-1)*(self.newtags_length)+(self.newtags_length)*(self.length-self.newtags_length)   
 			return (self.scores)
 	
-def avg_deg(input_file,output_file):
-	import simplejson,os
-	import pandas as pd      
+def Avg_degree_total(input_file,output_file):
 	twitter_input = open(input_file,"r")
 	f = open(output_file, 'w')
 	f.close()
-	
-	total_time = []
-	total_hash = []
+	import pandas as pd
+	from datetime import datetime
+	from datetime import timedelta
+	import simplejson
+	Tweettime = list()
+	Tweet_hashs = list()
 	def inputfile(twitter_input):
 		for line in twitter_input:        
 			single_tweet = simplejson.loads(line)
@@ -43,62 +45,45 @@ def avg_deg(input_file,output_file):
 					tags=[]
 					time_stamp = single_tweet['created_at']                
 					hash_tags = single_tweet["entities"]['hashtags']
-					total_time.append(time_stamp)
+					Tweettime.append(time_stamp)
 					for i in range(len(hash_tags)):
 						tags.append(hash_tags[i]["text"])
-					total_hash.append(tags) 
+					Tweet_hashs.append(tags) 
 					
-		return (total_time,total_hash)
-	data=pd.DataFrame(columns=('Time_stamp','Hashtags'))
-	data.Time_stamp,data.Hashtags=inputfile(twitter_input)
-	data.Time_stamp=pd.to_datetime(data.Time_stamp)
-	data=data.sort_values('Time_stamp')
-	data.index = range(0,data.shape[0])
-	def avg_score(score_val):
-		return((score_val)/float(len(unique_tags)))
-	def write_file(score_val):
+		return (Tweettime,Tweet_hashs)
+	Tweetdata=pd.DataFrame(columns=('Time_stamp','Hashtags'))
+	Tweetdata.Time_stamp,Tweetdata.Hashtags=inputfile(twitter_input)
+	Tweetdata.Time_stamp=pd.to_datetime(Tweetdata.Time_stamp)
+	Tweetdata=Tweetdata.sort_values('Time_stamp')
+	Tweetdata.index = range(0,Tweetdata.shape[0])
+	def Averagescore(score_value):
+		return((score_value)/float(len(UniCountertags)))
+	def write_file(score_value):
          with open(output_file,'a') as f:
-             f.write("{0:.2f}\n".format((score_val)))
-			 
-	prev_avg_score = []
-	#unique_tags = []
-	from datetime import timedelta
-	temp1 = pd.DataFrame(columns=('Time_stamp','Hashtags'))
-	score_val = 0
-	for j in range(data.shape[0]):
-		#print j
-		End_tag=data.Time_stamp[j]
-		Start = End_tag - timedelta(seconds=60)
-		temp1 = temp1.append(data.loc[j])
-		temp2= temp1[(temp1['Time_stamp']>Start) & (temp1['Time_stamp']<=End_tag)]
-		if((temp1.shape[0]-temp2.shape[0])>0):
-		   unique_tags[:] = []
-		   score_val=0
-		   temp1 = temp2
-		temp2.index = range(0,temp2.shape[0])
+             f.write("{0:.2f}\n".format((score_value)))
+	   
+	Paverage_score = list()
+	dataroll = pd.DataFrame(columns=('Time_stamp','Hashtags'))
+	score_value = 0
+	for j in range(Tweetdata.shape[0]):
+		Latest_tweet_time=Tweetdata.Time_stamp[j]
+		start_tweet_time = Latest_tweet_time - timedelta(seconds=60)
+		dataroll = dataroll.append(Tweetdata.loc[j])
+		fil_dataroll= dataroll[(dataroll['Time_stamp']>start_tweet_time) & (dataroll['Time_stamp']<=Latest_tweet_time)]
+		if((dataroll.shape[0]-fil_dataroll.shape[0])>0):
+		   UniCountertags[:] = []
+		   score_value=0
+		   dataroll = fil_dataroll
+		fil_dataroll.index = range(0,fil_dataroll.shape[0])
+		for i in range(fil_dataroll.shape[0]):
+			a=Tweetlvlscore(fil_dataroll.Time_stamp[i],fil_dataroll.Hashtags[i])
+			score_value = a.Tweetscore() + score_value
 		
-
-		
-		for i in range(temp2.shape[0]):
-			a=Calculate_score(temp2.Time_stamp[i],temp2.Hashtags[i])
-			score_val = a.score() + score_val
-		
-		if (len(unique_tags) == 0):
-			#print prev_avg_score
-			#print 'success'
-			write_file(round(prev_avg_score,2))  
+		if (len(UniCountertags) == 0):
+			write_file(round(Paverage_score,2))  
 		else:
-			#print avg_score(score_val) 
-			prev_avg_score = avg_score(score_val)
-			#print 'nsuccess'
-			write_file(round(prev_avg_score,2))
+			Paverage_score = Averagescore(score_value)
+			write_file(round(Paverage_score,2))
 
 if __name__ == '__main__':
-    import os
-    file_dir = os.path.dirname(os.path.realpath('__file__'))
-    print file_dir
-    input_file = file_dir + r'/tweet_input/tweets.txt'
-    print input_file
-    output_file = file_dir + r'/tweet_output/output.txt'
-    print output_file
-    avg_deg(input_file,output_file)
+    main()
